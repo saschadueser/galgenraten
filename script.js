58d24c9afed119f7d45d16c0e4e43859ddd849e5
saschadueser <saschadueser@gmail.com>
2025-11-15 07:48:36 +0100

    Grafik, Random-Words und kleine Designanpassung
    Galgen-Grafik hinzugefügt, Random-Word Generator erstellt und Wordlist erstellt. div. grafische Anpassungen.

--- script.js
+++ script.js
@@ -1,95 +1,135 @@
-// controls
-const wordToGuess = "Weihnachtsmann";
-let winningCounter = 0;
-let losingCounter = 0
-const winningCounterTarget = wordToGuess.length;
-const losingCounterTarget = 6;
-
-// elements
+// Controls - Dürfen bearbeitet werden
+let wordToGuess = ""; // Optional festlegbar, falls ein individuelles Wort erraten werden soll
+const losingCounterTarget = 6; // Festlegen der maximalen Fehlversuche
+ 
+// Counter
+let winningCounter = 0; // Counter zum Zählen der gefundenen Buchstaben
+let losingCounter = 0; // Counter zum Zählen der Fehlversuche
+let winningCounterTarget; // Automatisch ermittelt bei Festsetzung des gesuchten Wortes
+ 
+// HTML elements
 const keyboard = document.querySelector(".keyboard");
 const wantedWord = document.querySelector(".wanted-word");
-const hintBoxSuccess = document.querySelector(".hintbox .success-counter");
-const hintBoxFailure = document.querySelector(".hintbox .failure-counter");
+const hintBoxFailureCounter = document.querySelector(".hintbox .failure-counter");
+const hintBoxFailureContainer = document.querySelector(".hintbox .failure");
 let letterGuess = document.querySelectorAll(".letter-guess")
-
+ 
 // data
-const alpthabet = [];
-
+const alphabet = [];
+ 
 // One time run for creating the alphabet in letters array
 for (let i = 65; i <= 90; i++) {
-  alpthabet.push(String.fromCharCode(i));
+  alphabet.push(String.fromCharCode(i));
 }
-
-hintBoxFailure.textContent = `${losingCounter} / ${losingCounterTarget}`
-hintBoxSuccess.textContent = `${winningCounter} / ${winningCounterTarget}`
-
-
-
+ 
+// One time function for picking a random word from the list
+async function gameStart() {
+    if (wordToGuess === "") {
+        let fetchedWordList = await fetch("./words.json");
+        let fetchedWordListJson = await fetchedWordList.json();
+        wordToGuess = fetchedWordListJson[Math.floor(Math.random() * fetchedWordListJson.length)]
+    }
+    setTimeout(() => {
+        createWordToGuess()
+        gameLoad = true;
+        winningCounterTarget = wordToGuess.length;
+        hintBoxFailureCounter.textContent = `${losingCounter} / ${losingCounterTarget}`
+    }, 1);
+}
+ 
+gameStart();
+ 
 // Function for check the chosen letter
 function checkLetter(e, letter) {
     let tempArr = [];
-
+ 
     for (let x in wordToGuess) {
         if(wordToGuess[x].toLowerCase() === letter.toLowerCase() ) {
             tempArr.push(x);
         }
     }
-
+ 
     if(tempArr.length === 0) {
-        alert("Letter not found")
-        hintBoxFailure.textContent = `${++losingCounter} / ${losingCounterTarget}`
-    if(losingCounterTarget === losingCounter) {
-        alert("Game is over. You lose")
-        document.querySelectorAll(".letter").forEach( letterEl => {
-            letterEl.classList.add("inactive")
-        })
+        // Letter not found
+        hintBoxFailureContainer.classList.add("animate-wrong-answer")
+        setTimeout(() => {
+            hintBoxFailureContainer.classList.remove("animate-wrong-answer")
+        }, 500);
+        hintBoxFailureCounter.textContent = `${++losingCounter} / ${losingCounterTarget}`
+        drawPerson();
+ 
+        // Game Over - Losing State
+        if(losingCounterTarget === losingCounter) {
+            alert("Game is over. You lose")
+            document.querySelectorAll(".letter").forEach( letterEl => {
+                letterEl.classList.add("inactive")
+            })
+            wantedWord.innerHTML = wordToGuess.toUpperCase();
+            wantedWord.style.color = "red"
     }
     } else {
-        alert("Letter found")
+        // Letter found
+        wantedWord.classList.add("animate-correct-answer")
+        setTimeout(() => {
+            wantedWord.classList.remove("animate-correct-answer")
+        }, 500);
         tempArr.forEach( el => {
             letterGuess[el].textContent = letter
-            hintBoxSuccess.textContent = `${++winningCounter} / ${winningCounterTarget}`
+            winningCounter++;
         })
+ 
+        // Game Over - Winning State
         if(winningCounterTarget === winningCounter) {
             alert("Game is over. You won")
             document.querySelectorAll(".letter").forEach( letterEl => {
                 letterEl.classList.add("inactive")
             })
+            wantedWord.style.color = "green"
         }
     }
     // deactivate the letter key of the keyboard
     e.target.classList.add("inactive")
+    console.log(winningCounter)
 }
-
-
-
-
-
+ 
+//**
+// One time perform */
+ 
 // Create letter keys for the keyboard and attach an EventHandler to it
-alpthabet.forEach( letter => {
-    const newElement = document.createElement("div");
-    newElement.classList.add("letter");
-    newElement.textContent = letter;
-
-    newElement.addEventListener("click", (e) => {
-        checkLetter(e, letter)
+function createWordToGuess() {
+    alphabet.forEach( letter => {
+        const newElement = document.createElement("div");
+        newElement.classList.add("letter");
+        newElement.textContent = letter;
+ 
+        newElement.addEventListener("click", (e) => {
+            checkLetter(e, letter)
+        })
+ 
+        keyboard.appendChild(newElement)
     })
-
-    keyboard.appendChild(newElement)
-})
-
-
-
-
-
-
-
-// Create the missing word fields (runtime once)
-for(let x of wordToGuess) {
-    const letterPlaceholder = document.createElement("div");
-    letterPlaceholder.classList.add("letter-guess")
-    letterPlaceholder.textContent = "_"
-
-    wantedWord.appendChild(letterPlaceholder)
-    letterGuess = document.querySelectorAll(".letter-guess")
+ 
+    // Create the missing word fields (runtime once)
+    for(let x of wordToGuess) {
+        const letterPlaceholder = document.createElement("div");
+        letterPlaceholder.classList.add("letter-guess")
+        letterPlaceholder.textContent = "_"
+ 
+        wantedWord.appendChild(letterPlaceholder)
+        letterGuess = document.querySelectorAll(".letter-guess")
+    }
+ 
 }
+ 
+const graphicParts = document.querySelectorAll(".person");
+ 
+function drawPerson() {
+    for (let x = 0; x < losingCounter ; x++) {
+        graphicParts[x].style.display = "block";
+    }
+   
+}
+ 
+    graphicParts.forEach( el => {
+        el.style.display = "none"
+    })
